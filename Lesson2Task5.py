68c2c79bc3282e8eb7b4fe38bb2a84d7b2e994c5
Inna <innailiukova@gmail.com>
2023-03-13 16:55:16 +0300

    коментар

--- Lesson2Task5.py
+++ Lesson2Task5.py
@@ -1,5 +1,6 @@
 # Task 5
 # Написати скрипт, який повертає найбільше число серед трьох, які введені користувачем.
+
 n1 = int(input("Print number1 = "))
 n2 = int(input("Print number2 = "))
 n3 = int(input("Print number3 = "))
